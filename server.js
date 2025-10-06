fc8f258b50d6730148d941117091314234de8d12
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2025-10-06 09:53:21 -0300

    feat: Cria modelo base do Backend

---
+++ server.js
@@ -0,0 +1,25 @@
+// Importar pacotes/bibliotecas
+import express from "express";
+import dotenv from "dotenv"
+
+// Criar aplicação com Express e configurar para aceitar JSON
+const app = express();
+app.use(express.json());
+
+// Carregar variáveis de ambiente e definir constante para porta do servidor
+dotenv.config();
+const serverPort = process.env.PORT || 3001;
+
+// Rota principal GET para "/"
+app.get("/", (req, res) => {
+    res.send("🚀 Servidor funcionando...");
+});
+
+
+// Aqui vão todas suas Rotas
+
+
+// Iniciar servidor escutando na porta definida
+app.listen(serverPort, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
+});
